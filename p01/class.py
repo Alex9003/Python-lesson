861ea6d6eed9377bc727d3f1ecb472eeac3d2fc9
Alex <9003SAB>
2024-01-27 15:05:55 +0200

    l01

---
+++ p01/class.py
@@ -0,0 +1,10 @@
+print("Input number school: ")
+number_school=input();
+print("Input number your class: ")
+number_your_class=input();
+print("Input number all girls: ")
+number_all_girls=input();
+print("Input number all boys: ")
+number_all_boys=input();
+all_people=int(number_all_boys)+int(number_all_girls)
+print("Numder school %s \nNumber class %s\nNumber people %d" %(number_school,number_your_class,all_people))
---
+++ p01/test.py
@@ -0,0 +1,8 @@
+#a=unput
+print('---Start---')
+#v1
+print('Hello World!!!')
+print("Це новий рядок!!!!!")
+#v2
+print('Hello World!!!', "\nЦе новий рядок!!!!!")
+print("Part 1",'\nPart 2','\nPart 3','\nPart 4')
