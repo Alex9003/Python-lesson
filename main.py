34d8c33dfba7cfdbc36817f2f559508598911d5a
Alex <9003SAB>
2024-02-03 16:52:54 +0200

    l04

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+os.system('clear')
+
+#print('Text: %s Number: %d' %('Text', 123))
+#name = 'Jon' 
+#num = 123
+#print(f'Text {name}, Integer{num}')
+#print(20//7)
+#print(20%7)
+#print(20**7)
+#x=7
+#x-=10
+#print(x)
+a=int(input('Ведіть число:'))
+print(7==7)
+print(7>=7)
+print(7>7)
+answer='Error'
+if a>=5 :
+    if a<=10:
+        answer='ok'
+print(answer)
